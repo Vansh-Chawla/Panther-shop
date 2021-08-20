c1ddf214b390bc002fb2ef33d65dec9eaf97e1b1
Vansh Chawla <vansh@chawlas.co.in>
2021-08-20 21:22:19 +0530

    k

---
+++ src/PAT.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+
+/**
+ *
+ * @author Ashu
+ */
+public class PAT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrameLogIn form = new JFrameLogIn () ; // creates a nnew Log In JFrame
+        form.setVisible (true); // displays log in jFrame
+    }
+    
+}
